47bb50c5bc0c2d0d0d10ef9fdaa408ef2e67d8ef
Mariana Camacho <marianacamacho700@gmail.com>
2022-04-17 21:12:10 -0500

    quita linea innecesaria

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Header from "../components/Header.jsx";
 import { StyleGlobal } from "../styles/styleGlobal.js";
-import "../styles/styles.css"
 import CardList from "./CardList";
 function App() {
   return (
